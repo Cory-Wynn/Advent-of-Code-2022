93542812160c70236582d92886a48754808a671e
Cory Wynn <cory.wynn@asos.com>
2022-12-01 11:36:32 +0000

    Add project files.

---
+++ AoC/Days/Day1.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC.Days
+{
+    internal class Day1
+    {
+        public static int PartOne()
+        {
+            return CaloriesByElfList().MaxBy(x => x);
+        }
+
+        public static int PartTwo()
+        {
+            return CaloriesByElfList().OrderByDescending(x => x).Take(3).Sum();
+        }
+
+        private static IEnumerable<int> CaloriesByElfList()
+        {
+            var lines = File.ReadLines("C:\\Users\\cory.wynn\\source\\repos\\AoC\\AoC\\Input\\input.txt");
+
+            var tempList = new List<int>();
+            var caloriesByElfList = new List<int>();
+
+            foreach (var line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    tempList.Add(int.Parse(line));
+                }
+                else
+                {
+                    caloriesByElfList.Add(tempList.Sum());
+                    tempList = new List<int>();
+                }
+            }
+
+            return caloriesByElfList;
+        }
+    }
+}
---
+++ AoC/Program.cs
@@ -0,0 +1,13 @@
+﻿using AoC.Days;
+
+namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day1.PartOne());
+            Console.WriteLine(Day1.PartTwo());
+        }
+    }
+}
